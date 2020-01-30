2119caa2f9e74c33b116eb93d907dde7d43f6790
Dominik Suess <suess@adobe.com>
2020-01-30 16:54:47 +0100

    SLING-9041 - adjusting regexp for named capturing groups for runmode and pid and adjusting the matching logic to be used for pid detection as well.

--- src/main/java/org/apache/sling/feature/cpconverter/handlers/AbstractConfigurationEntryHandler.java
+++ src/main/java/org/apache/sling/feature/cpconverter/handlers/AbstractConfigurationEntryHandler.java
@@ -29,63 +29,65 @@ abstract class AbstractConfigurationEntryHandler extends AbstractRegexEntryHandl
     private static final String REPOINIT_PID = "org.apache.sling.jcr.repoinit.RepositoryInitializer";
 
     public AbstractConfigurationEntryHandler(String extension) {
-        super("/jcr_root/(?:apps|libs)/.+/config(\\.([^/]+))?/.+\\." + extension);
+        super("/jcr_root/(?:apps|libs)/.+/config(\\.(?<runmode>[^/]+))?/(?<pid>.*)\\." + extension);
     }
 
     @Override
     public final void handle(String path, Archive archive, Entry entry, ContentPackage2FeatureModelConverter converter) throws Exception {
-        String pid = entry.getName().substring(0, entry.getName().lastIndexOf('.'));
-
-        String factoryPid = null;
-        String id;
-        int n = pid.indexOf('~');
-        if (n == -1) {
-            n = pid.indexOf('-');
-        }
-        if (n > 0) {
-            factoryPid = pid.substring(0, n);
-            id = factoryPid.concat("~").concat(pid.substring(n + 1));
-        } else {
-            id = pid;
-        }
-
-        logger.info("Processing configuration '{}'.", id);
-
-        Dictionary<String, Object> configurationProperties;
-        try (InputStream input = archive.openInputStream(entry)) {
-            configurationProperties = parseConfiguration(id, input);
-        }
-
-        if (configurationProperties == null) {
-            logger.info("{} entry does not contain a valid OSGi configuration, treating it as a regular resource", path);
-            converter.getMainPackageAssembler().addEntry(path, archive, entry);
-            return;
-        }
 
         Matcher matcher = getPattern().matcher(path);
+        
         String runMode = null;
         // we are pretty sure it matches, here
         if (matcher.matches()) {
+            
+            String pid = matcher.group("pid");
+    
+            String factoryPid = null;
+            String id;
+            int n = pid.indexOf('~');
+            if (n == -1) {
+                n = pid.indexOf('-');
+            }
+            if (n > 0) {
+                factoryPid = pid.substring(0, n);
+                id = factoryPid.concat("~").concat(pid.substring(n + 1));
+            } else {
+                id = pid;
+            }
+    
+            logger.info("Processing configuration '{}'.", id);
+    
+            Dictionary<String, Object> configurationProperties;
+            try (InputStream input = archive.openInputStream(entry)) {
+                configurationProperties = parseConfiguration(id, input);
+            }
+    
+            if (configurationProperties == null) {
+                logger.info("{} entry does not contain a valid OSGi configuration, treating it as a regular resource", path);
+                converter.getMainPackageAssembler().addEntry(path, archive, entry);
+                return;
+            }
             // there is a specified RunMode
-            runMode = matcher.group(2);
+            runMode = matcher.group("runmode");
+            
+            if (REPOINIT_PID.equals(factoryPid)) {
+                String[] scripts = (String[]) configurationProperties.get("scripts");
+                if (scripts != null) {
+                    String text = String.join("\n", scripts);
+                    converter.getFeaturesManager().addOrAppendRepoInitExtension(text, runMode);
+                } else {
+                    // any repoinit configuration with empty scripts may be igored - filereferences are not supported at that point
+                }
+            } else {
+                converter.getFeaturesManager().addConfiguration(runMode, id, configurationProperties);
+            }
         } else {
             throw new IllegalStateException("Something went terribly wrong: pattern '"
                                             + getPattern().pattern()
                                             + "' should have matched already with path '"
                                             + path
                                             + "' but it does not, currently");
-        }
-        
-        if (REPOINIT_PID.equals(factoryPid)) {
-            String[] scripts = (String[]) configurationProperties.get("scripts");
-            if (scripts != null) {
-                String text = String.join("\n", scripts);
-                converter.getFeaturesManager().addOrAppendRepoInitExtension(text, runMode);
-            } else {
-                // any repoinit configuration with empty scripts may be igored - filereferences are not supported at that point
-            }
-        } else {
-            converter.getFeaturesManager().addConfiguration(runMode, id, configurationProperties);
         }
     }
 
