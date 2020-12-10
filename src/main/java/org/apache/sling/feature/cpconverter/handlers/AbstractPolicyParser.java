bf97c89f5a594a5332d232ca111354f05956d024
anchela <angela@apache.org>
2020-12-10 11:06:17 +0100

    SLING-9976 : Refactor RepPolicyEntryHandler to allow for other types of access control lists (#41)
    Co-authored-by: angela <anchela@adobe.com>

---
+++ src/main/java/org/apache/sling/feature/cpconverter/handlers/AbstractPolicyParser.java
@@ -0,0 +1,103 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.sling.feature.cpconverter.handlers;
+
+import org.apache.sling.feature.cpconverter.accesscontrol.AccessControlEntry;
+import org.apache.sling.feature.cpconverter.accesscontrol.AclManager;
+import org.apache.sling.feature.cpconverter.shared.AbstractJcrNodeParser;
+import org.apache.sling.feature.cpconverter.shared.RepoPath;
+import org.jetbrains.annotations.NotNull;
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+
+import javax.xml.transform.sax.TransformerHandler;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+abstract class AbstractPolicyParser extends AbstractJcrNodeParser<Boolean> {
+
+    static final String REP_RESTRICTIONS = "rep:Restrictions";
+    static final String REP_PRINCIPAL_NAME = "rep:principalName";
+
+    private static final String REP_PRIVILEGES = "rep:privileges";
+    private static final String[] RESTRICTIONS = new String[] { "rep:glob", "rep:ntNames", "rep:prefixes", "rep:itemNames" };
+
+    private static final Pattern typeIndicatorPattern = Pattern.compile("\\{[^\\}]+\\}\\[(.+)\\]");
+
+    private final RepoPath repositoryPath;
+
+    final TransformerHandler handler;
+    final AclManager aclManager;
+
+    boolean onRepAclNode = false;
+    // ACL processing result
+    boolean hasRejectedNodes = false;
+
+    public AbstractPolicyParser(@NotNull String primaryType, @NotNull RepoPath repositoryPath, @NotNull AclManager aclManager, @NotNull TransformerHandler handler) {
+        super(primaryType);
+        this.handler = handler;
+        this.repositoryPath = repositoryPath;
+        this.aclManager = aclManager;
+    }
+
+    private static String extractValue(String expression) {
+        if (expression == null || expression.isEmpty()) {
+            return expression;
+        }
+
+        Matcher matcher = typeIndicatorPattern.matcher(expression);
+        if (matcher.matches()) {
+            return matcher.group(1);
+        }
+
+        return expression;
+    }
+
+    static void addRestrictions(@NotNull AccessControlEntry ace, @NotNull Attributes attributes) {
+        for (String restriction : RESTRICTIONS) {
+            String v = extractValue(attributes.getValue(restriction));
+
+            if (v != null && !v.isEmpty()) {
+                ace.addRestriction(restriction + ',' + v);
+            }
+        }
+    }
+
+    AccessControlEntry createEntry(boolean isAllow, @NotNull Attributes attributes) {
+        return new AccessControlEntry(isAllow, extractValue(attributes.getValue(REP_PRIVILEGES)), repositoryPath);
+    }
+
+    @Override
+    public void startDocument() throws SAXException {
+        handler.startDocument();
+    }
+
+    @Override
+    public void endDocument() throws SAXException {
+        handler.endDocument();
+    }
+
+    @Override
+    protected void onJcrRootElement(String uri, String localName, String qName, Attributes attributes) {
+        onRepAclNode = true;
+    }
+
+    @Override
+    protected Boolean getParsingResult() {
+        return hasRejectedNodes;
+    }
+}
