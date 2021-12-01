f3d7f86f9684c9b3034926e8b6a1684d043a766a
Niek Raaijmakers <raaijmak@adobe.com>
2021-12-01 15:19:37 +0100

    SLING-10931 - Removing clutter, adding copyrights

--- src/main/java/org/apache/sling/feature/cpconverter/handlers/slinginitialcontent/xmlbuffer/XMLNode.java
+++ src/main/java/org/apache/sling/feature/cpconverter/handlers/slinginitialcontent/xmlbuffer/XMLNode.java
@@ -1,3 +1,19 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with this
+ * work for additional information regarding copyright ownership. The ASF
+ * licenses this file to You under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
 package org.apache.sling.feature.cpconverter.handlers.slinginitialcontent.xmlbuffer;
 
 import org.apache.sling.feature.cpconverter.vltpkg.VaultPackageAssembler;
@@ -8,6 +24,9 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.*;
 
+/**
+ * Represents an XML node in the buffer to be written in a sling initial content package
+ */
 public class XMLNode {
 
     private static final String FORMAT_SINGLE_VALUE = "%s";
